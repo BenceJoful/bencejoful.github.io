d7a72fcba5c2dc65ba3250c16b07af8e2f7a5f2c
BenceJoful <bencejoful@gmail.com>
2021-04-05 04:18:01 -0700

    Add files via upload

--- js/SLICYPlayer.js
+++ js/SLICYPlayer.js
@@ -2660,7 +2660,7 @@ $(document).ready(function () {
                 }
             }
         }
-        if (hashString(boardShading) == 110412525) {
+        if (hashString(boardShading) == -1877856181) {
             setTimeout(function () { alert("Congratulations, you solved the puzzle!") }, 1);
             isMouseDown = false;
         }
